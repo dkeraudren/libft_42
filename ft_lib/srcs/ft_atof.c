5c7661d0a1846b65c7fb5f2431c4e7048587d096
Damien KERAUDREN <dkeraudr@e1r1p7.clusters.42paris.fr>
2023-04-11 19:26:20 +0200

    fixed atof

--- ft_lib/srcs/ft_atof.c
+++ ft_lib/srcs/ft_atof.c
@@ -6,37 +6,53 @@
 /*   By: dkeraudr <dkeraudr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 17:44:43 by dkeraudr          #+#    #+#             */
-/*   Updated: 2023/04/10 11:04:53 by dkeraudr         ###   ########.fr       */
+/*   Updated: 2023/04/11 19:25:46 by dkeraudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_lib.h"
 
-double	get_decimal(const char *str)
+double	handle_exp(const char *str, double number)
 {
-	double	result;
-	double	decimal;
-	int		i;
+	int	i;
 
 	i = 0;
-	result = ft_atoi(str + i);
-	if (str[i] == '.')
-		i++;
-	decimal = ft_atoi(str + i);
-	while (decimal >= 1)
-		decimal /= 10;
-	result += decimal;
 	while (ft_isdigit(str[i]))
 		i++;
 	if (str[i] == 'e' || str[i] == 'E')
 	{
 		i++;
 		if (str[i] == '-')
-			result /= ft_pow(10, ft_atoi(str + i + 1));
+			number /= ft_pow(10, ft_atoi(str + i + 1));
 		else if (str[i] == '+')
-			result *= ft_pow(10, ft_atoi(str + i + 1));
+			number *= ft_pow(10, ft_atoi(str + i + 1));
+		else if (ft_isdigit(str[i]))
+			number *= ft_pow(10, ft_atoi(str + i));
 	}
-	return (result);
+	return (number);
+}
+
+double	get_decimal(const char *str)
+{
+	double	result;
+	double	decimal;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	result = ft_atoi(str + i);
+	while (ft_isdigit(str[i]))
+		i++;
+	if (str[i] == '.')
+		i++;
+	decimal = ft_atoi(str + i);
+	while (ft_isdigit(str[i + j]))
+		j++;
+	while (j--)
+		decimal /= 10;
+	result += decimal;
+	return (handle_exp(str + i, result));
 }
 
 double	ft_atof(const char *str)
