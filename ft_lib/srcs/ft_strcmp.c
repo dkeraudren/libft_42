c24190d8195d2b2b4f60aae66cebe31e2934b596
Damien KERAUDREN <dkeraudr@paul-f4Br3s2.clusters.42paris.fr>
2023-10-17 21:07:40 +0200

    Fixed ft_strcmp.c

--- ft_lib/srcs/ft_strcmp.c
+++ ft_lib/srcs/ft_strcmp.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp,c                                        :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dkeraudr <dkeraudr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 21:46:14 by dkeraudr          #+#    #+#             */
-/*   Updated: 2023/10/16 21:47:28 by dkeraudr         ###   ########.fr       */
+/*   Updated: 2023/10/17 19:27:33 by dkeraudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_lib.h"
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	return (ft_strncmp(s1, s2, get_max(ft_strlen(s1), ft_strlen(s2))));
+	return (ft_strncmp(s1, s2, ft_get_max(ft_strlen(s1), ft_strlen(s2))));
 }
