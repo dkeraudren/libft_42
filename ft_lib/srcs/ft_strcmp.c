a9e8be76f01c585c8ed7439a595bb3ee859aab44
Damien KERAUDREN <dkeraudr@made-f0Br8s9.clusters.42paris.fr>
2023-10-28 16:40:53 +0200

    Fixed strcmp

--- ft_lib/srcs/ft_strcmp.c
+++ ft_lib/srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: dkeraudr <dkeraudr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 21:46:14 by dkeraudr          #+#    #+#             */
-/*   Updated: 2023/10/17 19:27:33 by dkeraudr         ###   ########.fr       */
+/*   Updated: 2023/10/28 16:13:08 by dkeraudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	return (ft_strncmp(s1, s2, ft_get_max(ft_strlen(s1), ft_strlen(s2))));
+	size_t	len1;
+	size_t	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (len1 != len2)
+		return (1);
+	return (ft_strncmp(s1, s2, len1));
 }
